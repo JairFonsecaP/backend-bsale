c7421979b3d9c48a92b24814bbb7bb36eb1786a9
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-03-07 01:01:05 -0300

    se agrega cors para poder facilitar las peticiones

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 /*NECESARIO PARA PODER ENVIAR Y RECIBIR LOS JSON*/
 app.use(express.json());
